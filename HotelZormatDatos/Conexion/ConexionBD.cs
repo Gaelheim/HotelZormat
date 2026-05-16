feb52156c9816ad79a8eab1fc9a5c056c12f9353
Aleng <Aleng@ALENGUNI>
2026-05-16 18:49:35 -0400

    Add project files.

---
+++ HotelZormatDatos/Conexion/ConexionBD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelZormatDatos.Conexion
+{
+    public static class ConexionBD
+    {
+        private static readonly string _cadena =
+            "Data Source= .;Initial Catalog=HotelZormatDB; Integrated Security=True;";
+
+        public static SqlConnection ObtenerConexion()
+        { 
+
+            return new SqlConnection(_cadena);
+
+        }
+
+
+    }
+}
